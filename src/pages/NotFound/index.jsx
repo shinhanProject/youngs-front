6fdb7c454a372ae44e7cccf22ed039c516ca3cd8
chososo <80512711+chososo@users.noreply.github.com>
2023-10-11 23:28:33 +0900

    [#11] : refactor - notfound 코드 정리

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,11 +1,6 @@
-import "../../styles/reset.css";
-import { Footer } from "../../components";
-
 const NotFound = () => {
   return (
-    <Footer>
       <div>페이지가 존재하지 않습니다.</div>
-    </Footer>
   );
 };
 
