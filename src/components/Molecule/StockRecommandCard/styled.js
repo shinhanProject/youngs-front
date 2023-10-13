0d4302774366e4bf44d88be699e69754d5144348
chososo <a50242953@gmail.com>
2023-10-13 11:40:24 +0900

    [#30] feat : 급등 주식 추천 컴포넌트 생성

---
+++ src/components/Molecule/StockRecommandCard/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  border-bottom: 2px solid gray;
+  padding: 10px 0px 10px 0px;
+  margin: 0vw 2vw 0vw 2vw;
+`;
--- src/components/index.js
+++ src/components/index.js
@@ -12,7 +12,9 @@ export { default as Footer } from "./Molecule/Footer";
 export { default as LoginProfile } from "./Molecule/LoginProfile";
 export { default as InfoCategory } from "./Molecule/InfoCategory";
 export { default as SummaryCard } from "./Molecule/SummaryCard";
+export { default as StockRecommandCard } from "./Molecule/StockRecommandCard";
 
 export { default as Banner } from "./Organism/Banner";
 export { default as RankingModal } from "./Organism/RankingModal";
 export { default as GptModal } from "./Organism/GptModal";
+export { default as StockRecommand } from "./Organism/StockRecommand";
