3729edc9b3b05646c11e1dd31f4d815ffbdebf84
JZU0 <ljzh1030@gmail.com>
2023-10-13 21:30:44 +0900

    [#45] feat : add drag chatgpt

--- src/apis/index.js
+++ src/apis/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: `http://${process.env.REACT_APP_ENDPOINT}`,
-  timeout: 5000,
+  timeout: 15000,
   headers: {
     "Content-Type": "application/json",
   },
