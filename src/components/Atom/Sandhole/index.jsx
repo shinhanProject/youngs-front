7c817edc5136b575876ce85ec3f8d6cd0c43661c
chososo <a50242953@gmail.com>
2023-10-13 11:39:46 +0900

    [#11] style : 코드 수정

--- src/components/Atom/Sandhole/index.jsx
+++ src/components/Atom/Sandhole/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyledSandhole, Shells } from "./styled";
-import shell1 from "../../../assets/images/shell1.svg";
+// import shell1 from "../../../assets/images/shell1.svg";
 // src 로 shell1, shell2, shell3  바꿔서 넣으면 될 것 같습니다
 // 정답 맞춘 개수 받아서 shells 수정할 수 있도록 상태값 받기로 수정 ?
 
