5c52332c3fc85aaf4bc85effe57e58aacc0396af
chososo <a50242953@gmail.com>
2023-10-14 14:53:05 +0900

    [#11] style : 랭킹 페이지 모션 추가

--- src/components/Molecule/RankingCard/styled.js
+++ src/components/Molecule/RankingCard/styled.js
@@ -9,6 +9,17 @@ export const Wrapper = styled.div`
 
   width: 20vw;
 `;
+export const WrapperUsername = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+  width: 20vw;
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 export const Wrapper2 = styled.div`
   display: flex;
@@ -27,4 +38,7 @@ export const Container = styled.div`
   width: 90vw;
   grid-template-rows: repeat(auto-fill, minmax(100px, 1fr));
   border-bottom: 1px solid lightgray;
+  &:hover {
+    background-color: rgba(0, 70, 255, 0.1);
+  }
 `;
