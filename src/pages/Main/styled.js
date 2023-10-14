e4357e78d47078c12ea5dd08b96e6ab2962a0224
chososo <a50242953@gmail.com>
2023-10-14 14:52:06 +0900

    [#11] style : 디자인 변경

--- src/pages/Main/styled.js
+++ src/pages/Main/styled.js
@@ -13,16 +13,38 @@ export const Container = styled.div`
 export const DecoImg = styled.img`
   position: absolute;
   top: 30vh;
-  left: 15vw;
+  left: 10vw;
   width: 15vw;
-  z-index: 1;
-  filter: opacity(0.3);
+  z-index: -1;
+  filter: opacity(0.2);
 `;
 
 export const DecoImg2 = styled.img`
   position: absolute;
-  top: 63vh;
+  bottom: -15vh;
+  transform: rotate(55deg);
+  left: 14vw;
+  width: 35vw;
+  z-index: -1;
+  filter: opacity(0.1);
+`;
+export const DecoImg3 = styled.img`
+  position: absolute;
+  top: 2vh;
+  transform: rotate(55deg);
   right: 10vw;
-  width: 15vw;
-  z-index: 1;
+  width: 40vw;
+  z-index: -1;
+  filter: invert(15%) sepia(87%) saturate(7242%) hue-rotate(227deg)
+    brightness(103%) contrast(106%) opacity(0.2);
+`;
+export const DecoImg4 = styled.img`
+  position: absolute;
+  top: -28vh;
+  transform: rotate(33deg);
+  left: 20vw;
+  width: 25vw;
+  z-index: -1;
+  filter: invert(15%) sepia(87%) saturate(7242%) hue-rotate(227deg)
+    brightness(103%) contrast(106%) opacity(0.2);
 `;
